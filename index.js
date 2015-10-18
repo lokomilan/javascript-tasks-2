feb594204ef7395f1910795a7fd31ce0a1c1fe61
Alex Dubikovsky <lokomilan94@gmail.com>
2015-10-19 01:10:40 +0500

    Исправлены замечания

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
 phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
 
 // Невалидные данные не должны попадать в книгу!
-phoneBook.add('Честный Хрюндель', '+7 (999) 777-7-77', 'honest-hrundel@gmail.com');
+phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
 
 phoneBook.find('777');
 // Выводит построчно записи, все поля через запятую:
--- phoneBook.js
+++ phoneBook.js
@@ -2,25 +2,37 @@
 
 var phoneBook = [];
 
+function getSearchString(record) {
+    var searchString = '';
+    var keys = Object.keys(record);
+    keys.forEach(value => {
+        searchString += ', ' + String(record[value]);
+    });
+    //хотелось написать лямбду в одну строчку, но тогда не пройдет jscs
+    return searchString.substring(2);
+}
+
 function markProperRecords(query) {
     var properRecordsPositions = [];
+    if (query == null) {
+        return properRecordsPositions;
+    }
     for (var i = 0; i < phoneBook.length; i++) {
         var currentRecord = phoneBook[i];
-        var searchString = '';
-        for (var key in currentRecord) {
-            searchString = searchString + ' ' + String(currentRecord[key]);
-        }
-        if (searchString.lastIndexOf(query) >= 0) {
+        var searchString = getSearchString(currentRecord);
+        var queryRegExp = new RegExp(query, 'i');
+        if (queryRegExp.test(searchString)) {
             properRecordsPositions.push(i);
         }
     }
     return properRecordsPositions;
 }
-var emailRegExp = /^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@[a-zа-я0-9_-]+(\.[a-zа-я0-9_-]+)*\.[a-za-я]+$/i;
+
+var emailRegExp = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-zа-яё0-9_-]+(\.[a-zа-яё0-9_-]+)*\.[a-za-яё]+$/i;
 var phoneRegExp = /^(\+?\d+\s?)?((\d{3})|(\(\d{3}\)))(\s?)(\d{3})([\-\s])?(\d)([\-\s])?(\d{3})$/;
 
 module.exports.add = function add(name, phone, email) {
-    if (name && phoneRegExp.test(phone) && emailRegExp.test(email)) {
+    if (String(name) && phoneRegExp.test(phone) && emailRegExp.test(email)) {
         var record = {
             name: name,
             phone: phone,
@@ -33,12 +45,9 @@ module.exports.add = function add(name, phone, email) {
 module.exports.find = function find(query) {
     var positions = markProperRecords(query);
     for (var i = 0; i < positions.length; i++) {
-        var recordString = '';
-        var record = phoneBook[positions[i]];
-        for (var key in record) {
-            recordString = recordString + ', ' + record[key];
-        }
-        console.log(recordString.substring(2));
+        var currentRecord = phoneBook[positions[i]];
+        var recordString = getSearchString(currentRecord);
+        console.log(recordString);
     }
 };
 
@@ -49,4 +58,3 @@ module.exports.remove = function remove(query) {
     }
     console.log('Удалено контактов: ' + String(positions.length));
 };
-
